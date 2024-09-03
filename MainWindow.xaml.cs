dbd11c0760754bf49d4738da1f2f27d1f495e0d4
greyes625 <60638269+greyes625@users.noreply.github.com>
2024-09-03 17:37:08 +0800

    implemented login to main window screen and basic login method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,15 +25,15 @@ namespace LobbyCLient
         //Declare interface variables for file, lobby and room servers.
         private IFileServer fileInterface;
         private ILobbyServer lobbyInterface;
-        private IRoomServer roomInterface;
+        private IMessageServer messageInterface;
         public MainWindow()
         {
             InitializeComponent();
-
+            /*
             //Declare the channel factories for file, lobby and room channels.
             ChannelFactory<IFileServer> fileFactory;
             ChannelFactory<ILobbyServer> lobbyFactory;
-            ChannelFactory<IRoomServer> roomFactory;
+            ChannelFactory<IMessageServer> roomFactory;
 
             //Set up the connection
             NetTcpBinding tcp = new NetTcpBinding();
@@ -43,15 +43,56 @@ namespace LobbyCLient
             //Lobby and room to be duplex as goes both ways.
             fileFactory = new ChannelFactory<IFileServer>(tcp, URL);
             lobbyFactory = new DuplexChannelFactory<ILobbyServer>(tcp, URL);
-            roomFactory = new DuplexChannelFactory<IRoomServer>(tcp, URL);
+            roomFactory = new DuplexChannelFactory<IMessageServer>(tcp, URL);
 
             //Create the factory channels.
             fileInterface = fileFactory.CreateChannel();
             lobbyInterface = lobbyFactory.CreateChannel();
-            roomInterface = roomFactory.CreateChannel();
+            messageInterface = roomFactory.CreateChannel();
 
+            */
 
+            //Set main window as collapsed and login window as visible by default
+            mainScreen.Visibility = Visibility.Collapsed;
+            loginScreen.Visibility = Visibility.Visible;
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Set errorbox to hidden by default
+                ErrorBox.Visibility = Visibility.Collapsed;
+
+                //get username from usernameBox and try to join lobby.
+                if (usernameBox.Text.Equals("") || usernameBox.Text.Contains(" "))
+                {
+                    ErrorBox.Text = "Username not valid. Please try again.";
+                    ErrorBox.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    //collapse login screen and make main window visible
+                    loginScreen.Visibility = Visibility.Collapsed;
+                    mainScreen.Visibility = Visibility.Visible;
+                    userView.Text = usernameBox.Text;
+                }
+                
+
+            }
+            catch (Exception ex) 
+            { 
+                ErrorBox.Text = ex.Message;
+            }
+        }
+
+        private void logoutButton_Click(Object sender, RoutedEventArgs e)
+        {
+            //collapse main window and make login screen visible
+            mainScreen.Visibility = Visibility.Collapsed;
+            loginScreen.Visibility = Visibility.Visible;
 
         }
     }
+
 }
